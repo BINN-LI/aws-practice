00c1639965e56ba054dceac3faf0e213ade97429
BINN-LI <lhb951030@gmail.com>
2022-05-29 19:29:27 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  //test 2
+  //test
   imports: [],
   controllers: [AppController],
   providers: [AppService],
